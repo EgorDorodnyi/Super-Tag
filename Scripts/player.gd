2c70107e4d86645197703f2f1943b3562928247e
gallagerWSCW <159972359+gallagerWSCW@users.noreply.github.com>
2025-10-19 23:03:47 +1300

    Finishing the bounce pad and tilemap

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,19 +1,24 @@
 extends CharacterBody2D
 @onready var tag_icon = $TagIcon
+@onready var animated_sprite = $AnimatedSprite2D
 
+@onready var tilemap=$"../TileMapLayer3"
 
 var is_tagger: bool=false
+
 const SPEED = 150.0
 const JUMP_VELOCITY = -370.0
+const BOUNCE_VELOCITY = -770.0
 
 func _ready():
 	tag_icon.visible = false
 
-@onready var animated_sprite = $AnimatedSprite2D
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("player1_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -31,6 +36,20 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
+
+	#Bounce check
+	for i in get_slide_collision_count():
+		var collision := get_slide_collision(i)
+		if collision:
+			var collider := collision.get_collider()
+			if collider == tilemap:
+				velocity.y = BOUNCE_VELOCITY
+				break
+	
+	
+	
+
+
 #Tag icon
 #check if the tagger collide 
 func on_Area_body_entered(body):
--- Scripts/player_2blue.gd
+++ Scripts/player_2blue.gd
@@ -1,10 +1,15 @@
 extends CharacterBody2D
 @onready var tag_icon = $TagIcon
+@onready var tilemap=$"../TileMapLayer3"
+@onready var animated_sprite = $AnimatedSprite2D
+
+
 var is_tagger: bool=false
 const SPEED = 150.0
 const JUMP_VELOCITY = -370.0
 
-@onready var animated_sprite = $AnimatedSprite2D
+var BOUNCE_VELOCITY = -770.0
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -26,6 +31,19 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
+	
+		#Bounce check
+	for i in get_slide_collision_count():
+		var collision := get_slide_collision(i)
+		if collision:
+			var collider := collision.get_collider()
+			if collider == tilemap:
+				velocity.y = BOUNCE_VELOCITY
+				break
+
+	
+#Tag icon
+#check if the tagger collide 
 func _on_Area_body_entered(body):
 	if is_tagger and body is CharacterBody2D:
 		body.set_as_tagger(true)
