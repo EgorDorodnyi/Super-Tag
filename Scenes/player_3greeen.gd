3ff7f9981a22b977055d31a49e6568e8818f372e
gallagerWSCW <159972359+gallagerWSCW@users.noreply.github.com>
2025-10-21 12:23:24 +1300

    Update player_3greeen.gd

--- Scenes/player_3greeen.gd
+++ Scenes/player_3greeen.gd
@@ -1,25 +1,53 @@
 extends CharacterBody2D
+@onready var tag_icon = $TagIcon
+@onready var tilemap=$"../TileMapLayer3"
+@onready var animated_sprite = $AnimatedSprite2D
 
 
-const SPEED = 400.0
-const JUMP_VELOCITY = -400.0
+var is_tagger: bool=false
+const SPEED = 150.0
+const JUMP_VELOCITY = -370.0
 
+var BOUNCE_VELOCITY = -770.0
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("player4_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	# Get the input direction -1,0,1
+	var direction = Input.get_axis("player4_move_left", "player4_move_right")
+	#flip the sprite
+	if direction>0:
+		animated_sprite.flip_h = false
+	elif direction<0:
+		animated_sprite.flip_h =true
+	#apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+	
+		#Bounce check
+	for i in get_slide_collision_count():
+		var collision := get_slide_collision(i)
+		if collision:
+			var collider := collision.get_collider()
+			if collider == tilemap:
+				velocity.y = BOUNCE_VELOCITY
+				break
+
+	
+#Tag icon
+#check if the tagger collide 
+func _on_Area_body_entered(body):
+	if is_tagger and body is CharacterBody2D:
+		body.set_as_tagger(true)
+		set_as_tagger(false)
+func set_as_tagger(value: bool):
+	is_tagger = value
+	tag_icon.visible= value
