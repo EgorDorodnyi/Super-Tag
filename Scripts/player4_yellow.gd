43a145d3d5fb517c0a9db9dd12572f8ea672733b
gallagerWSCW <159972359+gallagerWSCW@users.noreply.github.com>
2025-10-23 16:17:24 +1300

    finished setting up the fourth character

---
+++ Scripts/player4_yellow.gd
@@ -0,0 +1,60 @@
+extends CharacterBody2D
+@onready var tag_icon = $TagIcon
+@onready var animated_sprite = $AnimatedSprite2D
+
+@onready var tilemap=$"../TileMapLayer3"
+
+var is_tagger: bool=false
+
+const SPEED = 150.0
+const JUMP_VELOCITY = -370.0
+const BOUNCE_VELOCITY = -770.0
+
+func _ready():
+	tag_icon.visible = false
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	
+	# Handle jump.
+	if Input.is_action_just_pressed("player4_jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction: -1, 0, 1
+	var direction = Input.get_axis("player4_move_left", "player4_move_right")
+	#Flip the sprite
+	if direction>0:
+		animated_sprite.flip_h = false
+	elif direction<0:
+		animated_sprite.flip_h = true
+	# Apply movement
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
+
+	#Bounce check
+	for i in get_slide_collision_count():
+		var collision := get_slide_collision(i)
+		if collision:
+			var collider := collision.get_collider()
+			if collider == tilemap:
+				velocity.y = BOUNCE_VELOCITY
+				break
+	
+	
+#Tag icon
+#check if the tagger collide 
+func on_Area_body_entered(body):
+	if is_tagger and body is CharacterBody2D:
+		body.set_as_tagger(true)
+		set_as_tagger(false)
+func set_as_tagger(value: bool):
+	is_tagger = value
+	tag_icon.visible = value
+
+	
